b63f655fcc79c8b63831bffc3ba4ff0f2ce4f82e
Jules <j.marshallmiami@yahoo.com>
2022-06-04 12:27:53 -0500

    socket.io commented out

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 const server = require("http").Server(app);
-const io = require('socket.io')(server)
+// const io = require('socket.io')(server);
 
 
 const exphbs = require("express-handlebars");
