d0ae7d18c0f71f409e569ea7b1635ca2b962b806
Jules <j.marshallmiami@yahoo.com>
2022-06-07 13:35:10 -0500

    new changes

--- src/models/gameid.js
+++ src/models/gameid.js
@@ -1,23 +1,32 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
 
-class GameId extends Model {}
+class GameId extends Model { }
 
 GameId.init({
-  gameId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    primaryKey: true,
-  },
-  player1_id:{
-    type: Sequelize.DataTypes.String(10)
-  },
-  player2_id:{
-    type: Sequelize.DataTypes.String(10)
-  },
+    gameId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+    },
+    player1_id: {
+        type: Sequelize.DataTypes.String(10),
+        unique: true
+    },
+    player2_id: {
+        type: Sequelize.DataTypes.String(10),
+        unique: true
+    },
+    player1_turn: {
+        type: Sequelize.DataTypes.Boolean,
+        allowNull: false,
+        default: true
+    }
 },
 {
-  freezeTableName: true,
-  
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "gameid"
 });
 
 module.exports = GameId;
