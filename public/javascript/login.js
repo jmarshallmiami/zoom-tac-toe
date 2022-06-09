9379873e6e354c9ec7c8b17b71c258738488d19e
Jules <j.marshallmiami@yahoo.com>
2022-06-09 13:01:26 -0500

    second attempt to push

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -29,7 +29,7 @@ async function signupFormHandler(event) {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (username && password) {
-    const response = await fetch("/api/id", {
+    const response = await fetch("/api/users", {
       method: "post",
       body: JSON.stringify({
         username,
